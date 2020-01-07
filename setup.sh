f257d3a600244f403bb733954eb2dfd838aea2e6
Will Crichton <wcrichto@cs.stanford.edu>
2020-01-06 16:21:07 -0800

    Also clone seaborn

--- setup.sh
+++ setup.sh
@@ -10,3 +10,5 @@ pip3 install -r requirements.txt
 jupyter nbextension enable inliner/notebook --user
 jupyter nbextension enable hide_input/main --user
 jupyter nbextension enable freeze/main --user
+
+git clone https://github.com/mwaskom/seaborn
