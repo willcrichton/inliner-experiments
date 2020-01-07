70e02b4adffdfd0224d518e1511a267181643bb9
Will Crichton <wcrichto@cs.stanford.edu>
2020-01-06 16:22:15 -0800

    And install seaborn

--- setup.sh
+++ setup.sh
@@ -11,4 +11,6 @@ jupyter nbextension enable inliner/notebook --user
 jupyter nbextension enable hide_input/main --user
 jupyter nbextension enable freeze/main --user
 
-git clone https://github.com/mwaskom/seaborn
+git clone -b v0.9.0 https://github.com/mwaskom/seaborn
+cd seaborn
+pip3 install -e .
